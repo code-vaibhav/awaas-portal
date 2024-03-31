6fa5764388120c7edce88a71b459e6e1592f3187
code-vaibhav <vgoyal20@iitk.ac.in>
2024-04-01 02:09:30 +0530

    added env variables

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBS2fHk659LnxBV6eXLKUM7VjZNwz-XpKo",
-  authDomain: "awaas-portal.firebaseapp.com",
-  projectId: "awaas-portal",
-  storageBucket: "awaas-portal.appspot.com",
-  messagingSenderId: "323723568514",
-  appId: "1:323723568514:web:f1b79bca7fc15e2fe39b9b",
-  measurementId: "G-BMBHCX13PG",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
